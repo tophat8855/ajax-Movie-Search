7dad989885da89c57c43f0d75c287d626e93d826
tophat8855 <hrfarley@gmail.com>
2015-01-27 17:46:44 -0800

    shows the info

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 $(document).ready(function() {
-  var searchURL = "http://www.omdbapi.com/?s="
+  var searchURL = "http://www.omdbapi.com/?s=";
+  var searchDetails = "http://www.omdbapi.com/?i=";
+  var movieId;
 
   function initialize() {
     $('body').append($('<h1>Movie Search</h1>'));
     $('body').append($('<input placeholder="title search"></input>'));
     $('body').append($('<ul>'));
     $('input').keypress(entering);
+    $('ul').on('click', 'li', showMovieInfo);
+    $('body').append($('<div>'))
   }
 
-  function listTheMovies(movies) {
-    var movieList = $('<li>');
+  function listTheMovies(movies, movieID) {
+    var movieList = $('<li>').data('imdbID', movieID);
     movieList.html(movies);
     $('ul').append(movieList);
   }
@@ -22,14 +26,33 @@ $(document).ready(function() {
       dataType: "json",
       type: "get"
     }).done(function (data) {
-      console.log(data.Search);
       for (var i = 0; i < data.Search.length; i++) {
-        listTheMovies(data.Search[i].Title);
+        listTheMovies(data.Search[i].Title, data.Search[i].imdbID);
       }
       $('input').val("");
     }).fail(function () {
-      alert("it failed :<");
+      alert("it failed :(");
     });
+  }
+
+  function showMovieInfo() {
+    $('div').html("");
+    $.ajax({
+      url: searchDetails + $(this).data('imdbID'),
+      dataType: "json",
+      type: "get"
+    }).done(function(data) {
+      console.log(data);
+      var moviePoster = data.Poster;
+      //$('body').append('<div>')
+      $('div').append("Year:" + data.Year);
+      $('div').append("Plot:" + data.Plot);
+      $('div').append(data.Title);
+      $('div').append($('<img src= ' + moviePoster + '></img>'));
+
+
+    });
+
   }
 
   initialize();
